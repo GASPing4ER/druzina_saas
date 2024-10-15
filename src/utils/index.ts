aaaff0c923e4bba6512254565b706ce16419fb61
GASPing4ER <simecgasper@gmail.com>
2024-10-15 16:19:29 +0200

    updated the getCompleteData function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -41,13 +41,59 @@ export const getCompleteData = (
   user: User
 ) => {
   console.log(values);
-  if (values.current_phase === "uredništvo") {
+  if (values.current_phase === "osnutek") {
+    return {
+      ...values,
+      current_phase: "/",
+      napredek: 0,
+      status: "v pripravi",
+      stanje: 0,
+      creatorId: user.id,
+      creator_name: `${user.user_metadata.firstName} ${user.user_metadata.lastName}`,
+    };
+  } else if (values.current_phase === "uredništvo") {
     return {
       ...values,
       current_phase: "urednistvo",
       napredek: 1,
       status: "v teku",
       stanje: 0,
+      creatorId: user.id,
+      creator_name: `${user.user_metadata.firstName} ${user.user_metadata.lastName}`,
+    };
+  } else if (values.current_phase === "oblikovanje") {
+    return {
+      ...values,
+      napredek: 2,
+      status: "v teku",
+      stanje: 20,
+      creatorId: user.id,
+      creator_name: `${user.user_metadata.firstName} ${user.user_metadata.lastName}`,
+    };
+  } else if (values.current_phase === "priprava za tisk") {
+    return {
+      ...values,
+      napredek: 3,
+      status: "v teku",
+      stanje: 40,
+      creatorId: user.id,
+      creator_name: `${user.user_metadata.firstName} ${user.user_metadata.lastName}`,
+    };
+  } else if (values.current_phase === "tisk") {
+    return {
+      ...values,
+      napredek: 4,
+      status: "v teku",
+      stanje: 60,
+      creatorId: user.id,
+      creator_name: `${user.user_metadata.firstName} ${user.user_metadata.lastName}`,
+    };
+  } else if (values.current_phase === "dostava") {
+    return {
+      ...values,
+      napredek: 4,
+      status: "v teku",
+      stanje: 80,
       creatorId: user.id,
       creator_name: `${user.user_metadata.firstName} ${user.user_metadata.lastName}`,
     };
