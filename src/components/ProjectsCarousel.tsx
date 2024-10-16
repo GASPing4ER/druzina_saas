3a6ed5250116126ee70f1d64e0da5d4693838b17
GASPing4ER <simecgasper@gmail.com>
2024-10-16 13:52:50 +0200

    carousel project link to the project

--- src/components/ProjectsCarousel.tsx
+++ src/components/ProjectsCarousel.tsx
@@ -5,6 +5,7 @@ import {
   carouselProjectUserColors,
 } from "@/constants";
 import { formatDate } from "@/utils";
+import Link from "next/link";
 
 const ProjectsCarousel = async ({ projects }: ProjectsCarouselProps) => {
   return (
@@ -12,41 +13,46 @@ const ProjectsCarousel = async ({ projects }: ProjectsCarouselProps) => {
       {projects.map((project, index) => {
         const colorIndex = index % 3;
         return (
-          <div key={project.id} className="inline-block">
-            <div
-              className="h-[175px] w-[300px] rounded-xl flex flex-col p-6 justify-between"
-              style={{
-                backgroundColor: `${carouselProjectBgColors[colorIndex]}`,
-              }}
-            >
-              <h2 className="text-white font-bold">{project.name}</h2>
-              <div className="flex flex-col gap-2">
-                <div className="flex justify-between items-center text-white">
-                  <p>Napredek</p>
-                  <p>{project.stanje} %</p>
+          <Link
+            key={project.id}
+            href={`/phase/${project.current_phase}/${project.id}`}
+          >
+            <div key={project.id} className="inline-block">
+              <div
+                className="h-[175px] w-[300px] rounded-xl flex flex-col p-6 justify-between"
+                style={{
+                  backgroundColor: `${carouselProjectBgColors[colorIndex]}`,
+                }}
+              >
+                <h2 className="text-white font-bold">{project.name}</h2>
+                <div className="flex flex-col gap-2">
+                  <div className="flex justify-between items-center text-white">
+                    <p>Napredek</p>
+                    <p>{project.stanje} %</p>
+                  </div>
+                  <ProgressBar stanje={project.stanje} />
                 </div>
-                <ProgressBar stanje={project.stanje} />
-              </div>
-              <div className="flex justify-between items-center">
-                <p className="text-sm text-white font-semibold bg-slate-300 py-1 px-4 rounded-xl">
-                  {formatDate(project.end_date)}
-                </p>
-                <div className="flex">
-                  {carouselProjectUserColors.map((color: string) => (
-                    <Image
-                      key={color}
-                      src="/icons/user.svg"
-                      alt="user"
-                      width={25}
-                      height={25}
-                      className="p-1 rounded-full"
-                      style={{ backgroundColor: color }}
-                    />
-                  ))}
+                <div className="flex justify-between items-center">
+                  <p className="text-sm text-white font-semibold bg-slate-300 py-1 px-4 rounded-xl">
+                    {formatDate(project.end_date)}
+                  </p>
+                  <div className="flex">
+                    {carouselProjectUserColors.map((color: string) => (
+                      <Image
+                        key={color}
+                        src="/icons/user.svg"
+                        alt="user"
+                        width={25}
+                        height={25}
+                        className="p-1 rounded-full"
+                        style={{ backgroundColor: color }}
+                      />
+                    ))}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
+          </Link>
         );
       })}
     </div>
