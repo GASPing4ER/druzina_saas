c94bb5979ae12e7cd45bc3a56ba9ef89cd7442ac
GASPing4ER <simecgasper@gmail.com>
2024-10-20 13:53:00 +0200

    small fix

--- src/app/(dashboard)/(phase)/urednistvo/[projectId]/page.tsx
+++ src/app/(dashboard)/(phase)/urednistvo/[projectId]/page.tsx
@@ -2,12 +2,7 @@ import { getUser } from "@/actions/auth";
 import { getFiles } from "@/actions/files";
 import { getProject } from "@/actions/projects";
 import { getTasksWithNames } from "@/actions/tasks";
-import {
-  NextPhaseModal,
-  ProgressBar,
-  ProjectDetails,
-  UtilityBox,
-} from "@/components";
+import { NextPhaseModal, ProjectDetails, UtilityBox } from "@/components";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { redirect } from "next/navigation";
 
@@ -44,32 +39,31 @@ const ProjectDetailsPage = async ({
     return (
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start px-12 py-6 bg-white text-slate-900">
         <ProjectDetails project={project} />
-        <div className="flex w-full gap-10">
-          <Tabs defaultValue="naloge" className="w-[60%]">
-            <TabsList>
-              <TabsTrigger value="naloge">Naloge</TabsTrigger>
-              <TabsTrigger value="datoteke">Datoteke</TabsTrigger>
-              <TabsTrigger value="aktivnosti">Aktivnosti</TabsTrigger>
-            </TabsList>
-            <TabsContent value="naloge">
-              <UtilityBox
-                type="naloge"
-                data={tasks}
-                projectId={projectId}
-                role={role}
-              />
-            </TabsContent>
-            <TabsContent value="datoteke">
-              <UtilityBox
-                type="datoteke"
-                data={files}
-                projectId={projectId}
-                role={role}
-              />
-            </TabsContent>
-            <TabsContent value="aktivnosti">Tukaj bodo aktivnosti.</TabsContent>
-          </Tabs>
-          <div className="flex-1 flex flex-col gap-2">
+        <Tabs defaultValue="naloge" className="w-full">
+          <TabsList>
+            <TabsTrigger value="naloge">Naloge</TabsTrigger>
+            <TabsTrigger value="datoteke">Datoteke</TabsTrigger>
+            <TabsTrigger value="aktivnosti">Aktivnosti</TabsTrigger>
+          </TabsList>
+          <TabsContent value="naloge">
+            <UtilityBox
+              type="naloge"
+              data={tasks}
+              projectId={projectId}
+              role={role}
+            />
+          </TabsContent>
+          <TabsContent value="datoteke">
+            <UtilityBox
+              type="datoteke"
+              data={files}
+              projectId={projectId}
+              role={role}
+            />
+          </TabsContent>
+          <TabsContent value="aktivnosti">Tukaj bodo aktivnosti.</TabsContent>
+        </Tabs>
+        {/* <div className="flex-1 flex flex-col gap-2">
             <div className="flex flex-col gap-2 border shadow-2xl rounded-xl p-4">
               <h2 className="text-black text-2xl">Še 6 dni</h2>
               <ProgressBar stanje={70} />
@@ -80,8 +74,7 @@ const ProjectDetailsPage = async ({
               projectId={projectId}
               role={role}
             />
-          </div>
-        </div>
+          </div> */}
 
         {/* <UtilityBox type="datoteke" data={tasks} projectId={project.id} /> */}
         {tasksCompleted && role === "superadmin" && (
