af2ca882cb6bec23d7f6f575e58974e636a3abac
GASPing4ER <simecgasper@gmail.com>
2025-01-02 20:49:39 +0100

    Login form give submit, error, message state

--- src/components/auth/LoginUserForm.tsx
+++ src/components/auth/LoginUserForm.tsx
@@ -3,6 +3,7 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import { useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import { Input } from "../ui/input";
@@ -22,26 +23,46 @@ import { loginUserSchema } from "@/types/schemas";
 import { LoginUserProps } from "@/types";
 
 const LoginUserForm = () => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+
   const form = useForm<z.infer<typeof loginUserSchema>>({
     resolver: zodResolver(loginUserSchema),
     defaultValues: {},
   });
 
-  // 2. Define a submit handler.
+  // Submit handler
   async function onSubmit(values: z.infer<typeof loginUserSchema>) {
+    setIsSubmitting(true);
+    setErrorMessage("");
+    setSuccessMessage("");
+
     try {
       const completeData: LoginUserProps = {
         ...values,
       };
 
       await login(completeData);
+      setSuccessMessage("Prijava uspešna!");
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
     } catch (error) {
-      return error;
+      setErrorMessage(
+        "Prijava ni uspela. Preverite podatke in poskusite znova."
+      );
+    } finally {
+      setIsSubmitting(false);
     }
   }
+
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+        {errorMessage && <p className="text-red-600 text-sm">{errorMessage}</p>}
+        {successMessage && (
+          <p className="text-green-600 text-sm">{successMessage}</p>
+        )}
+
         <div className="flex gap-2">
           <div className="flex-1">
             <FormField
@@ -80,7 +101,10 @@ const LoginUserForm = () => {
             />
           </div>
         </div>
-        <Button type="submit">Prijavi se</Button>
+
+        <Button type="submit" disabled={isSubmitting}>
+          {isSubmitting ? "Prijavljam..." : "Prijavi se"}
+        </Button>
       </form>
     </Form>
   );
