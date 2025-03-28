fc75fe1c7fd7da4c596175be520a38767cdb72fe
GASPing4ER <simecgasper@gmail.com>
2025-03-28 17:05:01 +0100

    querying improved

--- src/components/project/ProjectsTable.tsx
+++ src/components/project/ProjectsTable.tsx
@@ -38,10 +38,17 @@ const ProjectsTable = ({ projects, is_arhiv = false }: ProjectsTableProps) => {
     query === "" && selectedType === "all"
       ? projects // Show all projects when both filters are at default values
       : projects.filter((project) => {
+          let full_name;
+          if (project.st_izdaje) {
+            full_name = `${project.name} ${project.st_izdaje}`;
+            console.log(full_name);
+          }
           const matchesQuery =
             query === "" ||
             project.name.toLowerCase().includes(query.toLowerCase()) ||
-            project.st_izdaje?.toLowerCase().includes(query.toLowerCase());
+            project.st_izdaje?.toLowerCase().includes(query.toLowerCase()) ||
+            full_name?.toLowerCase().includes(query.toLowerCase());
+
           const matchesType =
             selectedType === "all" || project.type === selectedType;
           return matchesQuery && matchesType; // Apply both filters if necessary
